55cc436f282b42981780b71ea4afd4dfd821e37c
dani <danifromecuador@gmail.com>
2023-07-31 13:18:42 -0500

    Create a hardcode array of air pollution details

---
+++ src/components/array.js
@@ -0,0 +1,37 @@
+const array = [
+  {
+    id: 0,
+    city: "New York City",
+    country: "USA",
+    airQuality: "Good",
+  },
+  {
+    id: 1,
+    city: "Paris",
+    country: "France",
+    airQuality: "Moderate",
+  },
+  {
+    id: 2,
+    city: "London",
+    country: "England",
+    airQuality: "Good",
+  },
+  {
+    id: 3,
+    city: "Tokyo",
+    country: "Japan",
+    airQuality: "Good",
+  },
+  {
+    id: 4,
+    city: "Beijing",
+    country: "China",
+    airQuality: "Unhealthy",
+  },
+};
+
+export default array;
+
+
+
