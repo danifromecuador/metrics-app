b8931e7062a4fb622471fad47832164cd31a4eb6
dani <danifromecuador@gmail.com>
2023-07-31 17:55:29 -0500

    Fix linter errors

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,27 +1,35 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { fetchItems } from '../redux/items/itemsSlice.js';
 import '../styles/Item.css';
 
 const Item = ({ id, city, country, airQuality }) => {
   const dispatch = useDispatch();
-  const onClick = (event) => {
-    event.preventDefault();
-    console.log(event.currentTarget);
-  };
+  // const onClick = (event) => {
+  //   event.preventDefault();
+  //   console.log(event.currentTarget);
+  // };
 
   useEffect(() => {
     dispatch(fetchItems());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <Link to={`/details/${id}`} className="item" onClick={onClick}>
+    <Link to={`/details/${id}`} className="item">
       <h2>{city}</h2>
       <h3>{country}</h3>
       <p>{airQuality}</p>
     </Link>
   );
+};
+
+Item.propTypes = {
+  id: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  airQuality: PropTypes.string.isRequired,
 };
 
 export default Item;
