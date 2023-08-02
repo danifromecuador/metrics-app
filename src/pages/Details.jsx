73d9dd1d116dc7eaadca24c63532fe5978578b18
dani <danifromecuador@gmail.com>
2023-08-01 22:30:17 -0500

    Fetch data when the user reloads the Details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,33 +1,17 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchItems } from '../redux/items/itemsSlice';
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import Header from '../components/Header';
-import '../styles/Details.css';
+import Header from "../components/Header";
+import '../styles/Details.css'
 
 const Details = () => {
-  const dispatch = useDispatch();
   const { city } = useParams();
-
-  useEffect(() => {
-    // Fetch the data and wait for it to complete before rendering the component
-    const fetchData = async () => {
-      try {
-        await dispatch(fetchItems());
-      } catch (error) {
-        // Handle errors if necessary
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, [dispatch, city]);
-
   const items = useSelector((state) => state.cities.items);
   const selectedArrayItem = items.find((item) => item.city === city);
 
-  // Render loading or error state if needed
+  useEffect(() => {
+  }, [city]);
   if (!selectedArrayItem) {
     return <div>Loading...</div>;
   }
@@ -38,7 +22,6 @@ const Details = () => {
       <div className="details">
         <h2>{selectedArrayItem.city}</h2>
         <div className="details-container">
-          {/* Render the details */}
           <p>carbon monoxide {selectedArrayItem.components.co}</p>
           <p>nitrogen monoxide {selectedArrayItem.components.no}</p>
           <p>nitrogen dioxide {selectedArrayItem.components.no2}</p>
@@ -54,5 +37,3 @@ const Details = () => {
 };
 
 export default Details;
-
-// Path: src/pages/Details.jsx
