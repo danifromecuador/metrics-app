83b46ba4868e2546a944c1e06806c0575c47351e
dani <danifromecuador@gmail.com>
2023-08-03 09:47:17 -0500

    Add all the elements to the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,25 @@ import { Link } from 'react-router-dom';
 import '../styles/Header.css'
 
 const Header = () => {
+  let hide = false;
   return (
     <header>
-      <Link to="/" className="return-arrow">
-        return
-      </Link>
-      <h1>HEADER</h1>
+      <div className="left">
+        <Link to="/" className="return-arrow">
+          &lt;
+        </Link>
+        <div className="year">
+          2022
+        </div>
+      </div>
+      <div className="center">
+        <h1 className='h1-title'>Air Pollution App</h1>
+        <h2 className="h2-title">City Details</h2>
+      </div>
+      <div className="right">
+        <div className="microphone">micro</div>
+        <div className="configuration">config</div>
+      </div>
     </header>
   );
 }
