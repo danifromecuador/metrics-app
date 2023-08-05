39e5d415c101bac5686e9d5c0a613734f1fc544a
dani <danifromecuador@gmail.com>
2023-08-05 18:25:29 -0500

    Change structure of tests

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Item = ({ city, carbonMonoxide, ozone, handleOnClick }) => {  
+const Item = ({ id, city, carbonMonoxide, ozone, handleOnClick }) => {  
   return (
-    <Link to={`/details/${city}`} className="item" onClick={handleOnClick}>
+    <Link to={`/details/${city}`} className="item" onClick={handleOnClick} id={id}>
       <h2 className='home-city-name'>{city}</h2>
       <div className="home-city-details">
         <p className='home-detail'>Carbon Monoxide Concentration: {carbonMonoxide}</p>
@@ -20,6 +20,7 @@ Item.propTypes = {
   city: PropTypes.string.isRequired,
   carbonMonoxide: PropTypes.number.isRequired,
   ozone: PropTypes.number.isRequired,
+  handleOnClick: PropTypes.func.isRequired,
 };
 
 export default Item;
--- src/test/Home.test.jsx
+++ src/test/Home.test.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line */
 import React from 'react';
-import { render, screen, debug } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux'; // Import Provider
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/store'; // Import your Redux store
@@ -8,54 +8,63 @@ import Home from '../pages/Home';
 import Input from '../components/Input';
 import Item from '../components/Item';
 
-test('renders home page title', () => {
-  const home = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </Provider>
-  );
-  // home.debug();
-  const titleElement = screen.getByText(/Air Pollution App/i);
-  expect(titleElement).toBeInTheDocument();
+describe('Home page', () => {
+  it('renders home page title', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>
+    );
+    const titleElement = screen.getByText(/Air Pollution App/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('displays the correct placeholder text', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Input />
+        </BrowserRouter>
+      </Provider>
+    );
+    const inputElement = screen.getByPlaceholderText(/Search city.../i);
+    expect(inputElement).toBeInTheDocument();
+  });
 });
 
-test('displays the correct placeholder text', () => {
-  const input = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Input />
-      </BrowserRouter>
-    </Provider>
-  );
-  // input.debug();
-  const inputElement = screen.getByPlaceholderText(/Search city.../i);
-  expect(inputElement).toBeInTheDocument();
-});
+describe('Item component', () => {
+  it('renders with correct data', () => {
+    const mockItem = {
+      id: 1,
+      city: 'Mock City',
+      components: {
+        co: 0,
+        o3: 0,
+      },
+    };
 
-test('renders Item component with correct data', () => {
-  const mockItem = {
-    city: 'Mock City',
-    components: {
-      co: 0,
-      o3: 0,
-    },
-  };
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Item
+            id={mockItem.id}
+            city={mockItem.city}
+            carbonMonoxide={mockItem.components.co}
+            ozone={mockItem.components.o3}
+            handleOnClick={() => null}
+          />
+        </BrowserRouter>
+      </Provider>
+    );
 
-  const input = render(
-    <Provider store={store}>
-      <BrowserRouter>
-      <Item city={mockItem.city} carbonMonoxide={mockItem.components.co} ozone={mockItem.components.o3} />
-      </BrowserRouter>
-    </Provider>
-  );
+    const cityNameElement = screen.getByText(mockItem.city);
+    const carbonMonoxideElement = screen.getByText(`Carbon Monoxide Concentration: ${mockItem.components.co}`);
+    const ozoneElement = screen.getByText(`Ozone Concentration: ${mockItem.components.o3}`);
 
-  const cityNameElement = screen.getByText(mockItem.city);
-  const carbonMonoxideElement = screen.getByText(`Carbon Monoxide Concentration: ${mockItem.components.co}`);
-  const ozoneElement = screen.getByText(`Ozone Concentration: ${mockItem.components.o3}`);
-
-  expect(cityNameElement).toBeInTheDocument();
-  expect(carbonMonoxideElement).toBeInTheDocument();
-  expect(ozoneElement).toBeInTheDocument();
+    expect(cityNameElement).toBeInTheDocument();
+    expect(carbonMonoxideElement).toBeInTheDocument();
+    expect(ozoneElement).toBeInTheDocument();
+  });
 });
