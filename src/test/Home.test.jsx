b94ce13d6b9a4a767715a9a7aa750518981e11ac
dani <danifromecuador@gmail.com>
2023-08-05 15:38:25 -0500

    Add test for Input component

--- src/test/Home.test.jsx
+++ src/test/Home.test.jsx
@@ -1,19 +1,34 @@
 /* eslint-disable-next-line */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, debug } from '@testing-library/react';
 import { Provider } from 'react-redux'; // Import Provider
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/store'; // Import your Redux store
 import Home from '../pages/Home';
+import Input from '../components/Input';
 
 test('renders home page title', () => {
-  render(
-    <Provider store={store}> {/* Wrap Home in Provider */}
+  const home = render(
+    <Provider store={store}>
       <BrowserRouter>
         <Home />
       </BrowserRouter>
     </Provider>
   );
+  // home.debug();
   const titleElement = screen.getByText(/Air Pollution App/i);
   expect(titleElement).toBeInTheDocument();
+});
+
+test('displays the correct placeholder text', () => {
+  const input = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Input />
+      </BrowserRouter>
+    </Provider>
+  );
+  // input.debug();
+  const inputElement = screen.getByPlaceholderText(/Search city.../i);
+  expect(inputElement).toBeInTheDocument();
 });
