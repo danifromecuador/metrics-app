f1acb0327874ff484130e73029a857fcc6b39222
dani <danifromecuador@gmail.com>
2023-08-06 00:47:33 -0500

    Fix linter error

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,4 +1,3 @@
-
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux"; import { Link } from 'react-router-dom';
 import { IoIosArrowBack, IoMdMic } from 'react-icons/io';
