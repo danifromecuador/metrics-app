52448640f036c26d893181436fa89146e6461075
dani <danifromecuador@gmail.com>
2023-08-05 22:30:29 -0500

    Fix liters errors

--- src/test/Items.test.jsx
+++ src/test/Items.test.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 /* eslint-disable-next-line */
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Items from '../components/Items';
